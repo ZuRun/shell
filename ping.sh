edecfb873705292c82798ca512aef5fc8ae307fa
ZuRun <sky_iv@sina.com>
2018-06-25 09:04:38 +0800

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # ping命令,带上时间,-c命令自己修改,示例指定10天
-# 建议nohup sh ./ping.sh >ping1.log & 后台运行
+# 建议nohup sh ./ping.sh >ping.log & 后台运行
 ping qq.com -c 864000 | awk '{ print $0"\t" strftime("%Y-%m-%d %H:%M:%S",systime()) } '
